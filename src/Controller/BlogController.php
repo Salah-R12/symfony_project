77900b4ecb1242bb3a73a5c56ea08d72a308bc5f
Salah <radi.salah12@gmail.com>
2020-04-01 14:16:57 +0200

    get composant databse

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,19 @@
+<?php
+// src/Controller/LuckyController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+class BlogController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction()
+    {
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+        ]);
+    }
+}
